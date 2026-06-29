930f24009067075e150c7268f552e99b6d80b910
ndil <rahasia2alpha@gmail.com>
2026-06-29 16:51:42 +0700

    feat(brand): update shadcn base components ke Onetone tokens

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -19,10 +19,13 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-header"
-      className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
-        className
-      )}
+      // className={cn(
+      //   "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+      //   className
+      // )}
+
+      className="flex flex-col space-y-1.5 p-6"
+
       {...props}
     />
   )
@@ -32,7 +35,8 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-      className={cn("leading-none font-semibold", className)}
+      // className={cn("leading-none font-semibold", className)}
+      className="text-lg font-semibold text-foreground"
       {...props}
     />
   )
